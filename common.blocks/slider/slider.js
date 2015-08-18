44b7643d86adbeb3424d4083d60e4e830c9cc564
adinvadim <adin.vadim@gmail.com>
2015-08-18 23:47:00 +0200

    Slider, next onlick

--- common.blocks/slider/slider.js
+++ common.blocks/slider/slider.js
@@ -5,13 +5,37 @@ provide(BEMDOM.decl(this.name,
 	onSetMod: {
 		'js': {
 			'inited': function() {
-				this._active = this.findElem('item', 'active', true);
+				this.activeIndex = this.findElem('item', 'active', true).index();
+				this.bindTo('click', this.next);
 
-				this.fineElem('item').css('background-image', this.params.url);
+				var items = this.findElem('item');
+				for (var i = 0; i < items.length; i++) {
+					items[i].style.backgroundImage = "url(" + this.elemParams($(items[i])).url + ")";
+				}
+			},
+		},
+
+		'active':  {
+			true: function() {
+				console.log('lol');
+				//$(this.findElem('content')).animate({'opacity': '1'});
 			}
 		}
 	},
+
 	next: function() {
+		var items = this.elem('item'),
+			active = this.elem('item', 'active', true);
+
+		if (this.activeIndex < items.length - 1) {
+			this.activeIndex += 1;
+		} else {
+			this.activeIndex = 0;
+		}
+
+		this.delMod(active, 'active')
+			.setMod($(items[this.activeIndex]), 'active');
+
 		return;
 	}
 }));
